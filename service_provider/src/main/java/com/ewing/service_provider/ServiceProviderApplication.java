c1490e6993ab2142a596617e81f1d84c92078c8c
zyy <zyy_13579@163.com>
2019-05-05 18:30:22 +0800

    完成配置文件手动更新

--- service_provider/src/main/java/com/ewing/service_provider/ServiceProviderApplication.java
+++ service_provider/src/main/java/com/ewing/service_provider/ServiceProviderApplication.java
@@ -2,8 +2,10 @@ package com.ewing.service_provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@RefreshScope
 @SpringBootApplication
 @EnableEurekaClient
 public class ServiceProviderApplication {
